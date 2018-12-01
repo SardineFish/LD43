e27bafdf9d92512c93b7dccc2dd01924a6937152
SardineFish <Sardine@live.cn>
2018-12-01 20:30:59 +0800

    Add GameServer project

---
+++ GameServer/LD43GameServer/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.WebSockets;
+
+namespace LD43GameServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .AllowCredentials());
+
+            app.UseStaticFiles();
+            app.UseWebSockets();
+            app.UseMvc();
+            app.Use(async (context, next) =>
+            {
+                if (context.WebSockets.IsWebSocketRequest)
+                {
+                    if (context.Request.Path == "/ws")
+                    {
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    }
+                    else
+                    {
+                        await next();
+                    }
+                }
+
+            });
+        }
+    }
+}
